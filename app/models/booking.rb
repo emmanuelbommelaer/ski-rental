1b43364e3d981f20ec317d656888bd70e068b621
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-27 16:26:44 +0100

    booking display

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :product
 
   validates :status, presence: true
-  validates :start_date, presence: true, comparison: { greater_than: DateTime.current }
+  validates :start_date, presence: true, comparison: { greater_than: Date.tomorrow.midnight }
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
 end
