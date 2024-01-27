1130c69934f68c6baa77edcb5790c60977c1fe9f
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-27 14:19:20 +0100

    products created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products do
+    resources :bookings, only: [:new, :create]
+  end
+
 end
