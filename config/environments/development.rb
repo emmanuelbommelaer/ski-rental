f49143640f2767172a230dc59f8f1fcf08cc7d0a
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-27 17:27:41 +0100

    fix cloudinary setup issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Store uploaded files on cloudinary (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
