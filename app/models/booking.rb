ee284fe77496211ebed95fa34ec5cf473c419679
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-02-01 21:04:00 +0100

    route name change + booking status validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: %w(Pending Accepted Rejected) }
   validates :start_date, presence: true, comparison: { greater_than: Date.tomorrow.midnight }
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
 end
